90c320f459f8556724a1937e9cc709081a0b2f3b
Nickolai Venturini <nicktellah5600@yahoo.com>
2024-01-30 12:34:37 -0500

    JS Vault Combination

---
+++ script.js
@@ -0,0 +1,19 @@
+// 1. Create alert and add this text "You have received this message because you have been chosen to open an important vault. Here is the secret combination"
+// 2. Create 3 constants named "str 1" , "str2" and "str3"
+// 3. 
+
+
+
+
+
+
+
+alert(
+    'You have received this message because you have been chosen to open an important vault. Here is the secret combination:');
+const str1 = 15 - 5; // : Subtraction
+const str2 = 20 * 2; // : Multiplication
+const str3 = 38 + 1; // : Addition
+
+// Alert Message for Vault Combo!
+alert("Vault Combination: " + str1 + " - " + str2 + " - " + str3);
+
